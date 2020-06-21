a01cb5649591d5cf02782eef01b6d2a0424f8f08
aprilandjan <maybreezes1988@gmail.com>
2020-06-21 21:32:55 +0800

    feat: use module bindings to automatically find addon

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
-const addon = require('./build/Release/binding.node');
+// const addon = require('./build/Release/binding.node');
+const addon = require('bindings')('binding.node');
 
 
 //  just export what is needed
